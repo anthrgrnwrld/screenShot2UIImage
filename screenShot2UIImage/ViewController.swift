81a721dbfb46072708e2d1bcfd524529b419e1a0
anthrgrnwrld <anthrgrnwrld@gmail.com>
2015-10-19 07:57:42 +0900

    1st commit

--- screenShot2UIImage/ViewController.swift
+++ screenShot2UIImage/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageScreenShot: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +20,40 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    /**
+    スクリーンキャプチャ用関数
+    
+    :returns: UIImage
+    */
+    func GetImage() -> UIImage {
+        
+        // キャプチャする範囲を取得.
+        let rect = self.view.bounds
+        
+        // ビットマップ画像のcontextを作成.
+        UIGraphicsBeginImageContextWithOptions(rect.size, false, 0.0)
+        let context: CGContextRef = UIGraphicsGetCurrentContext()!
+        
+        // 対象のview内の描画をcontextに複写する.
+        self.view.layer.renderInContext(context)
+        
+        // 現在のcontextのビットマップをUIImageとして取得.
+        let capturedImage : UIImage = UIGraphicsGetImageFromCurrentImageContext()
+        
+        // contextを閉じる.
+        UIGraphicsEndImageContext()
+        
+        return capturedImage
+    }
+
+    @IBAction func pressScreenShotButton(sender: AnyObject) {
+        
+        let capturedImage = GetImage() as UIImage     // キャプチャ画像を取得.
+        
+        imageScreenShot.image = capturedImage
+        
     }
 
 
